6eac8cd25a4ca8ccbc9a4e368b791a28d02ac055
cneluhena <129386035+cneluhena@users.noreply.github.com>
2024-01-02 00:41:51 +0530

    Input Text Box created

--- lib/main.dart
+++ lib/main.dart
@@ -9,11 +9,79 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
-        title: const Text('My Name is Chamod'),
-      ),
-    ));
+    return const MaterialApp(
+      home: TextBox(),
+    );
+  }
+}
+
+class TextBox extends StatefulWidget {
+  const TextBox({super.key});
+
+  @override
+  State<TextBox> createState() => _TextBoxState();
+}
+
+class _TextBoxState extends State<TextBox> {
+  final myController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    myController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double objectWidth = screenWidth * 0.75;
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('G10A'),
+        ),
+        body: Center(
+            child: SizedBox(
+          width: objectWidth,
+          child: Column(
+            children: [
+              TextField(
+                controller: myController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter the sim number',
+                ),
+              ),
+              const SubmitButton()
+            ],
+          ),
+        )));
+  }
+}
+
+class SubmitButton extends StatefulWidget {
+  const SubmitButton({super.key});
+
+  @override
+  State<SubmitButton> createState() => _SubmitButtonState();
+}
+
+class _SubmitButtonState extends State<SubmitButton> {
+  bool isClicked = false;
+  String buttonText = 'Clicked';
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          setState(() {
+            isClicked = !isClicked;
+            if (isClicked) {
+              buttonText = 'Not Clicked';
+            } else {
+              buttonText = 'Clicked';
+            }
+          });
+        },
+        child: Text(buttonText));
   }
 }
